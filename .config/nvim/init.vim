c674e4076446c44cb365fbb61d8f0da5bcd90974
CuteCat-UwU <usedtolovewindows@101>
2021-08-19 12:36:41 +0530

    lol

---
+++ .config/nvim/init.vim
@@ -0,0 +1,22 @@
+"init.vim
+
+lua <<EOF
+require('plugins')
+require('lsp_list')
+require('lspconf')
+require('aerialconf') 
+require('autocomplete')
+require('snippy')
+require('TSconf')
+require('FlotTerm')
+require('telescoping')
+require('shady')
+require("BarAndTab")
+require('DiscordFlex')
+EOF
+
+"sourcing
+source $HOME/.config/nvim/config/nvimconfig.vim
+source $HOME/.config/nvim/config/bars.vim
+source $HOME/.config/nvim/config/luatree.vim
+
---
+++ .config/nvim/lua/TSconf.lua
@@ -0,0 +1,50 @@
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ignore_install = { "javascript" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    disable = { "c", "rust" },  -- list of language that will be disabled
+    additional_vim_regex_highlighting = false,
+  },
+  highlight = {
+    enable = true,
+    custom_captures = {
+      -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+      ["foo.bar"] = "Identifier",
+    },
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true
+  },
+}
+
+--treesitter textobjects
+require'nvim-treesitter.configs'.setup {
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim 
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+
+        -- Or you can define your own textobjects like this
+        ["iF"] = {
+          python = "(function_definition) @function",
+          cpp = "(function_definition) @function",
+          c = "(function_definition) @function",
+          java = "(method_declaration) @function",
+        },
+      },
+    },
+  },
+}
+
